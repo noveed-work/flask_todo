5cea4171480b0636135f4aa0d4409f6a479b3daf
Noveedgithub <noveed.muhammed@hotmail.com>
2024-09-16 20:04:40 +0100

    updated config

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ docker network create new-network
 docker build -t todo-app .
 docker build -t mynginx -f Dockerfile.nginx .
 docker run -d --name todo-app --network new-network todo-app:latest
-docker run -d -p 80:80 --name mynginx --network new-network mynginx:latest
+docker run -d -p 5000:5000 --name mynginx --network new-network mynginx:latest
