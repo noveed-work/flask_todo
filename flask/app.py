f315c374568642ae4bc7bc48f17af37086f8fe3d
Noveedgithub <noveed.muhammed@hotmail.com>
2024-09-16 16:50:38 +0100

    first commit

---
+++ flask/app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.String(500), nullable = False)
+    created = db.Column(db.DateTime, default = datetime.now())
+    
+    def __repr__(self) -> str:
+        return f"{self.task_id} - {self.title}"
+
+@app.route("/", methods=['GET','POST'])
+def hello_world():
+    if request.method=='POST':
+        todo_title= request.form['title']
+        todo_desc= request.form['desc']
+        data = Todo(title=todo_title, description=todo_desc)
+        db.session.add(data)
+        db.session.commit()
+    alltodo = Todo.query.all()
+    return render_template('index.html',alltodo=alltodo)
+
+@app.route('/update/<int:task_id>', methods=['GET', 'POST'])
+def update(task_id):
+    if request.method=='POST':
+        todo_title= request.form['title']
+        todo_desc= request.form['desc']
+        data = Todo.query.filter_by(task_id=task_id).first()
+        data.title= todo_title
+        data.description= todo_desc
+        db.session.add(data)
+        db.session.commit()
+        return redirect('/')
+        
+    todo = Todo.query.filter_by(task_id=task_id).first()
+    return render_template('update.html',todo=todo)
+
+@app.route('/delete/<int:task_id>')
+def delete(task_id):
+    todo = Todo.query.filter_by(task_id=task_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0', port=5500, debug =True)
---
+++ run.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/bash
+docker rm -f docker ps -aq
+docker rmi -f images
+docker network create new-network
+docker build -t todo-app .
+docker build -t mynginx -f Dockerfile.nginx .
+docker run -d --name todo-app --network new-network todo-app:latest
+docker run -d -p 80:80 --name mynginx --network new-network mynginx:latest
