769e2b0e96274a3b396d998e37ad6112c32ca6e9
noveed-work <159810170+noveed-work@users.noreply.github.com>
2024-09-20 09:55:13 +0100

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/bash
-docker rm -f docker ps -aq
-docker rmi -f images
-docker network create new-network
-docker build -t todo-app .
-docker build -t mynginx -f Dockerfile.nginx .
-docker run -d --name todo-app --network new-network todo-app:latest
-docker run -d -p 5000:5000 --name mynginx --network new-network mynginx:latest
